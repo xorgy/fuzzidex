65b91bc2511277f5f0705121f7738da5e835fa88
Aaron Muir Hamilton <aaron@correspondwith.me>
2022-10-15 07:57:40 -0600

    Add new 'vary' API, and InvertIndex object.

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
 declare module "fuzzidex" {
   export function mkgrams(s: string, depth?: number): Set<string>;
+  export function mkgrams_vary(varier: (s: string) => string[], s: string, depth?: number): Set<string>;
   export function insert(index: object, key: any, index_string: string, depth?: number): object;
+  export function insert_vary(varier: (s: string) => string[], index: object, key: any, index_string: string, depth?: number): object;
   export function query(index: object, query_string: string, depth?: number): Map<any, number>;
+  export function query_vary(varier: (s: string) => string[], index: object, query_string: string, depth?: number): Map<any, number>;
+  export function covary(vsi: string[], variers: ((s: string) => string[])[]): Iterable<string>;
+  export function vary_obvious(s: string): Iterable<string>;
+  export class InvertIndex {
+    constructor ({depth, varier}?: {depth?: number, varier?: (s: string) => Iterable<string>});
+    insert(k: any, index_string: string): void;
+    query(query_string: string): Map<any, number>;
+  }
 }
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+const vary_ident = x => [x];
+
 export const mkgrams = (s, depth = 6) => {
+  return mkgrams_vary(vary_ident, s, depth);
+};
+
+export const mkgrams_vary = (varier, s, depth = 6) => {
   const gs = new Set();
-  const norm = s.normalize("NFD").toLowerCase();
-  const len = norm.length;
-  const rdepth = Math.min(depth, len);
-  for (let i = 0; i < len; i++)
-    for (let j = 0; j < rdepth; j++)
-      gs.add(norm.substring(i, i + j + 1))
+  for (const variant of varier(s)) {
+    const norm = variant.normalize("NFD");
+    const len = norm.length;
+    const rdepth = Math.min(depth, len);
+    for (let i = 0; i < len; i++)
+      for (let j = 0; j < rdepth; j++)
+        gs.add(norm.substring(i, i + j + 1))
+  }
   return gs;
 };
 
-export const insert = (index = {}, key, index_string, depth = 6) => {
-  const gs = mkgrams(index_string);
+export const insert = (index = {}, key, index_string, depth = 6) => insert_vary(vary_ident, index, key, index_string, depth);
+
+export const insert_vary = (varier, index = {}, key, index_string, depth = 6) => {
+  const gs = mkgrams_vary(varier, index_string, depth);
   for (const gram of gs) {
     if (!(gram in index))
       index[gram] = [];
@@ -20,8 +30,10 @@ export const insert = (index = {}, key, index_string, depth = 6) => {
   return index;
 }
 
-export const query = (index, query_string, depth = 6) => {
-  const gs = mkgrams(query_string);
+export const query = (index, query_string, depth = 6) => query_vary(vary_ident, index, query_string, depth);
+
+export const query_vary = (varier, index, query_string, depth = 6) => {
+  const gs = mkgrams_vary(varier, query_string, depth);
   const outmap = new Map();
   for (const gram of gs)
     if (gram in index)
@@ -29,3 +41,43 @@ export const query = (index, query_string, depth = 6) => {
         outmap.set(k, (outmap.get(k) | 0) + 1)
   return outmap;
 };
+
+export const covary = (vsi, variers) => {
+  const vs = new Set(vsi);
+  for (const s of [...vs])
+    for (const varier of variers)
+      for (const variant of varier(s)) {
+        vs.add(variant);
+        for (const varier2 of variers.filter(x => x !== varier))
+          for (const variant2 of varier2(variant))
+            vs.add(variant2);
+      }
+  return vs
+}
+
+export const vary_obvious = s => covary([s, `\x02${s}\x03`], [
+  x => [x.normalize("NFKD")],
+  x => [x.toLowerCase()],
+  x => [x.replace(/\s/gu, '')],     // without whitespace
+  x => [x.replace(/\p{P}/gu, '')],  // without punctuation
+  x => [x.replace(/\p{Sc}/gu, '')], // without currency symbols
+]);
+
+export class InvertIndex {
+  constructor ({depth, varier} = {}) {
+    this.idx = {};
+    this.depth = 6;
+    this.varier = vary_obvious;
+
+    if (depth) this.depth = depth;
+    if (varier) this.varier = varier;
+  }
+
+  insert(k, index_string) {
+    insert_vary(this.varier, this.idx, k, index_string, this.depth);
+  }
+
+  query(query_string) {
+    return query_vary(this.varier, this.idx, query_string, this.depth);
+  }
+}
